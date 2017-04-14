6b7fbfbed9640ffe62f6294f2276cf4eb2344536
John Hill <nouveau.pg@gmail.com>
2017-04-14 18:52:02 -0400

    Initial Commit

---
+++ WhiteNoise/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WhiteNoise
+//
+//  Created by John Hill on 4/14/17.
+//  Copyright © 2017 John Hill. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
